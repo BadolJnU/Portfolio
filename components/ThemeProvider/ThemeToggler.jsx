56cc060ca9a4e3a5d5d4bb447e1a308bf8deff1e
unknown <kbadol135@gmail.com>
2024-04-03 01:57:30 -0400

    bug fixing

--- components/ThemeProvider/ThemeToggler.jsx
+++ components/ThemeProvider/ThemeToggler.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from './../ui/button'
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
+import {MoonIcon, SunIcon} from '@radix-ui/react-icons'
 import { useTheme } from 'next-themes'
 
 const ThemeToggler = () => {
